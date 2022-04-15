7bdf2d0a5de14e86c5a022d35bf73847b8421199
byggemandboesen <victorboesen@hotmail.com>
2022-04-15 11:32:45 +0200

    :bug: Fixed degree interval not allowing for small floats

--- H-line.py
+++ H-line.py
@@ -1,21 +1,14 @@
+import contextlib
 import os, sys, json
 from time import sleep
 from datetime import datetime, timedelta
 
-# Due to Receive.py not wanting to be importet we add src to path
+# Import necessary classes/modules
 sys.path.append("src/")
 from rtl import RTL
 from plot import Plotter
 from Ephem import Coordinates
 from dsp import DSP
-
-
-# Reads user config
-def read_config():
-    path = 'config.json'
-    config = open(path, 'r')
-    parsed_config = json.load(config)
-    main(parsed_config)
 
 
 # Main method
@@ -38,10 +31,9 @@ def main(config):
     # If user wants 24h observations
     if OBSERVATION_PARAM["24h"]:
         # Checks if 360 is divisable with the degree interval and calculates number of collections
-        # TODO Doesn't work with float degree intervals
         try:
             deg_interval = OBSERVATION_PARAM["degree_interval"]
-            num_data = int(360/deg_interval) if 360 % deg_interval == 0 else ValueError
+            num_data = int(360/deg_interval) if (360/deg_interval).is_integer() else ValueError
             second_interval = 24*60**2/num_data
         except:
             print("Degree interval not divisable with 360 degrees")
@@ -77,11 +69,12 @@ def main(config):
         current_time = datetime.utcnow()
         for i in range (num_data):
             print(f"Started observing! - {datetime.utcnow()}")
-            observe(DSP_CLASS, SDR_PARAM, DSP_PARAM, PLOTTING_PARAM, OBSERVATION_PARAM["debug"], sdr, ra_list[i], dec, observer_velocity)
+            with contextlib.redirect_stdout(None):
+                observe(DSP_CLASS, SDR_PARAM, DSP_PARAM, PLOTTING_PARAM, OBSERVATION_PARAM["debug"], sdr, ra_list[i], dec, observer_velocity)
             print(f"Done observing! - {datetime.utcnow()}")
 
             # Wait for next execution
-            clear_console()
+            # clear_console()
             end_time = current_time + timedelta(seconds = second_interval * (i + 1))
             time_remaining = end_time - datetime.utcnow()
             print(f'Waiting for next data collection in {time_remaining.total_seconds()} seconds')
@@ -90,56 +83,12 @@ def main(config):
         # TODO Generate GIF or something
     else:
         print(f"Started observing! - {datetime.utcnow()}")
-        observe(DSP_CLASS, SDR_PARAM, DSP_PARAM, PLOTTING_PARAM, OBSERVATION_PARAM["debug"], sdr, ra, dec, observer_velocity)
+        with contextlib.redirect_stdout(None):
+            observe(DSP_CLASS, SDR_PARAM, DSP_PARAM, PLOTTING_PARAM, OBSERVATION_PARAM["debug"], sdr, ra, dec, observer_velocity)
         print(f"Done observing! - {datetime.utcnow()}")
 
 
-'''
-    if float(num_data).is_integer():
-        # Set coordinates for each observation if possible
-        if 0.0 == lat == lon == alt == az:
-            ra, dec = 'none', 'none'
-            observer_velocity = 'N/A'
-        else:
-            # Get current equatorial and galactic coordinates of antenna RA and Declination
-            Coordinates_class = Coordinates(lat = lat, lon = lon, alt = alt, az = az)
-            ra, dec = Coordinates_class.equatorial(num_data, args.interval)
-            gal_lat, gal_lon = Coordinates_class.galactic()
-            observer_velocity = Coordinates_class.observer_velocity(gal_lat, gal_lon)
-        
-        # Current time of first data collection
-        current_time = datetime.utcnow()
-        num_data = int(num_data)
-        
-        # Check if one or multiple observations are planned
-        if num_data == 0:
-            # Perform only ONE observation
-            freqs, data = observe(args)
-            if args.debug:
-                write_debug(freqs, data, args, ra, dec)
-            plot(freqs, data, ra, dec, low_y, high_y, observer_velocity)
-        else:
-            # Perform multiple observations for 24 hours
-            for i in range (num_data):
-                freqs, data = observe(args)
-                plot(freqs, data, ra[i], dec, low_y, high_y, observer_velocity)
-
-                # Wait for next execution
-                clear_console()
-                end_time = current_time + timedelta(seconds = second_interval * (i + 1))
-                time_remaining = end_time - datetime.utcnow()
-                print(f'Waiting for next data collection in {time_remaining.total_seconds()} seconds')
-                sleep(time_remaining.total_seconds())
-            
-            # Generate GIF from observations
-            Plot_class = Plot(freqs = freqs, data = data, observer_velocity = observer_velocity)
-            Plot_class.generate_GIF(ra[0], dec)
-            
-    else:
-        print('360 must be divisable with the degree interval. Eg. the quotient must be a positive natural number (1, 2, 3, and not 3.4)')
-        quit()
-'''
-
+# Perform observation
 def observe(DSP_CLASS, SDR_PARAM, DSP_PARAM, PLOTTING_PARAM, debug, sdr, ra, dec, observer_velocity):
     freqs = DSP_CLASS.generateFreqs(sample_rate = SDR_PARAM["sample_rate"])
     h_line_data = DSP_CLASS.sample(sdr)
@@ -191,8 +140,18 @@ def writeDebug(freqs, SNR_spectrum, h_line_spectrum, blank_spectrum, SDR_PARAM, 
         json.dump(json_file, file, indent = 4)
 
 
+# Reads user config
+def read_config():
+    path = 'config.json'
+    config = open(path, 'r')
+    parsed_config = json.load(config)
+    main(parsed_config)
+
+
+# For clearing console
 def clear_console():
     os.system('cls' if os.name =='nt' else 'clear')
+
 
 if __name__ == "__main__":
     read_config()
